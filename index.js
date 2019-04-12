dd246832f734fe1a5fa90d4f05b2ea923b257b4b
Alex-King <alex.king@ku.edu>
2019-04-12 15:29:20 -0500

    added some example exercises

---
+++ index.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+const app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'you loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+const app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript'},
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+
+const app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
