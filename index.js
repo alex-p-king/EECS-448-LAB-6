0b08010609c17028dec35622ba53470941c75c6f
Alex King <ktmking611@gmail.com>
2019-04-12 18:56:18 -0500

    app now pushes a list of objects and displays the entire list on the website

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const app = new Vue({
+let bStopInfo = [];
+let app = new Vue({
     el: '#app',
     data: {
-        todos: [
-            {message: 'Learn JavaScript'},
-            {message: 'Learn Vue' },
-            {message: 'Build something awesome' }
-        ]
-    }
+        stops: bStopInfo
+    },
+    mounted: fetch("https://utils.pauliankline.com/stops.json")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(myJson){
+            for(let i = 0; i < myJson.length; i++)
+            {
+                bStopInfo.push(myJson[i])
+            }
+            
+            console.log(myJson);
+            
+            
+            
+            
+        })
 })
