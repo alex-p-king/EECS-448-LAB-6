d4d7269773d18b630544e9f06927b7f34f20e82e
Alex King <ktmking611@gmail.com>
2019-04-14 19:46:40 -0500

    geolocation now collects users latitude and longitude

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ let app = new Vue({
     el: '#app',
     data: {
         stops: [],
-        numStops: 10
+        numStops: 10,
+        userLocation: null
     },
     computed: {
         filteredStops: function () {
-            console.log("filtered stops function runnign")
+
             let arr = []
             for (let i = 0; i < this.numStops; i++) {
-                //console.log(this.stops[i])
                 arr.push(this.stops[i])
             }
-            console.log(arr);
             return arr;
         }
     },
@@ -23,13 +22,39 @@ let app = new Vue({
         .then(function (myJson) {
             for (let i = 0; i < myJson.length; i++) {
                 app.stops.push(myJson[i]);
-                //console.log(myJson[i]);
             }
-
-            //console.log(myJson);
-
-
-
-
-        }),
+        })
+        .then(function (){
+            navigator.geolocation.getCurrentPosition(onPositionReceived);
+        })
 })
+function onPositionReceived(position){
+    console.log(position);
+    let lat = position.coords.latitude
+    let lon = position.coords.longitude
+    console.log(lat);
+    console.log(lon);
+    
+}
+//www.geodatasource.com/developers/javascript*/
+function calculateDistance(uLat,uLon,sLat,sLon){
+    if(uLat === sLat && uLon === sLon ){
+        return 0;
+    }
+    else{
+        let r_uLat = (Math.PI * uLat/180);
+        let r_sLat = (Math.PI * sLat/180);
+        let theta = uLon-sLon;
+        let r_theta = Math.PI * theta/180;
+        let distance = Math.sin(r_uLat)*Math.sin(r_sLat) + Math.cos(r_uLat)*Math.cos(r_sLat)*Math.cos(r_theta);
+        if(distance > 1){
+            distance = 1
+        }
+        distance = math.acos(distance);
+        distance = distance*(180/Math.PI);
+        distance = distance*60*1.1515*1.609344
+        return distance;
+    }
+}
+
+
